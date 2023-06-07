3fa29eb0613969010e3d5176a3081596c2deb3a3
garrettpants <garrettsvance@gmail.com>
2023-06-07 13:29:59 -0600

    added initial changes to html

--- public/login.js
+++ public/login.js
@@ -1,7 +1,42 @@
-function login() {
-    const nameE1 = document.querySelector("#name");
-    console.log(nameE1.value)
-
-    localStorage.setItem("userName", nameE1.value);
-    window.location.href = "waterstatus.html";
+(async () => {
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    document.querySelector('#name').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
   }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'waterstatus.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}

4d2229d4066881b08c0f092b37e7354debdce6aa
garrettpants <garrettsvance@gmail.com>
2023-06-13 17:41:19 -0600

    created app.css and .jsx

---
+++ src/app.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+    return (
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              Simon<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <a className='nav-link' href='index.html'>
+                  Home
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='waterstatus.html'>
+                  Check Status
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='recommendations.html'>
+                  Recommendations
+                </a>
+              </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <main>App components go here</main>
+  
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Garrett Vance</span>
+            <a className='text-reset' href='https://github.com/garrettsvance/startup'>
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
